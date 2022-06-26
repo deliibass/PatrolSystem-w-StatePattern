2a46d5ee409a29ac092337a8c2b56922eef9aac0
Oğuzhan Delibaş <64430254+deliibass@users.noreply.github.com>
2022-06-26 12:58:26 +0300

    V 1

---
+++ Assets/Scripts/AI.cs
@@ -0,0 +1,26 @@
+using System.Diagnostics;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AI : MonoBehaviour
+{
+    NavMeshAgent agent;
+    Animator anim;
+    public Transform player;
+    State currentState;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+        currentState = new Idle(gameObject, agent, anim, player);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentState = currentState.Process();
+    }
+}
---
+++ Assets/Scripts/GameEnvironment.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public sealed class GameEnvironment
+{
+    private static GameEnvironment instance;
+    private List<GameObject> checkpoints = new List<GameObject>();
+    public List<GameObject> Checkpoints { get { return checkpoints; } }
+
+    public static GameEnvironment Singelton
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new GameEnvironment();
+                instance.Checkpoints.AddRange(GameObject.FindGameObjectsWithTag("Checkpoint"));
+
+                instance.checkpoints = instance.checkpoints.OrderBy(wayPoint => wayPoint.name).ToList();
+            }
+            return instance;
+        }
+    }
+}
